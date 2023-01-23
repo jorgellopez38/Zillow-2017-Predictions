5658f043e0f55625c1b001976e6b34de6e513063
Jorge <jorge.l.lopez38@gmail.com>
2023-01-23 11:36:57 -0600

    explore.py

--- explore.py
+++ explore.py
@@ -1,5 +1,6 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 def plot_q1(train):
     '''this function plots the scatter plot for tax value and house sqft'''
@@ -35,3 +36,81 @@ def plot_q4(train):
     sns.barplot(x='bedrooms', y='tax_value', data=train.sample(500))
     plt.title('Bedrooms vs Tax Value')
     plt.show()
+    
+def q2_stats_test(train):
+    
+    '''this function which takes in a train data set and calculates and returns a specific 
+       t-test for the mean of bathroom data'''
+    
+    # setting alpha
+    a = 0.05
+    # creating a mean of bathrooms
+    avg_bath = train['bathrooms'].mean()
+    
+    # using the mean to create mask data frames on either side
+    above_bath = train[train.bathrooms >avg_bath].bathrooms
+    below_bath = train[train.bathrooms <= avg_bath].bathrooms
+    
+    # performing a t test
+    t, p = stats.ttest_ind(above_bath, below_bath, equal_var=False)
+
+    # if statement to return our results
+    if p / 2 > a:
+        print("We fail to reject null hypothesis")
+    elif t < 0:
+        print("We fail to reject null hypothesis")
+    else:
+        print("We reject the null hypothesis")
+        
+    print(f"t: {t}")
+    print(f"p: {p}")
+    
+def q4_stats_test(train):
+    '''this function which takes in a train data set and calculates and returns a specific 
+       t-test for the mean of bedroom data'''
+    a = 0.05
+    # creating an average metric of bedroom count
+    avg_bed = train['bedrooms'].mean()
+    
+    # creating a mask for a new data frame
+    abov_bed_sample = train[train.bedrooms > avg_bed].bedrooms
+    
+    # perform a t test
+    t, p = stats.ttest_1samp(abov_bed_sample , avg_bed)
+    
+    # if statement to return our results
+    if p > a:
+        print('We fail to reject null hypothesis: There is not a significant difference in the mean')
+    else:
+        print("We reject null hypothesis: There is some significant difference in the mean")
+        
+    print(f"t: {t}")
+    print(f"p: {p}")
+
+def q1_stats_test(train):
+    
+    '''a function which takes in a train data set and calculates and returns a specific 
+       t-test for the mean of house sqft data'''
+    
+    # setting alpha
+    a = 0.05
+    # creating a mean of house sqft
+    avg_house_sqft = train['house_sqft'].mean()
+    
+    # using the mean to create mask data frames on either side
+    above_house_sqft = train[train.house_sqft >avg_house_sqft].house_sqft
+    below_house_sqft = train[train.house_sqft <= avg_house_sqft].house_sqft
+    
+    # performing a t test
+    t, p = stats.ttest_ind(above_house_sqft, below_house_sqft, equal_var=False)
+
+    # if statement to return our results
+    if p / 2 > a:
+        print("We fail to reject null hypothesis")
+    elif t < 0:
+        print("We fail to reject null hypothesis")
+    else:
+        print("We reject the null hypothesis")
+        
+    print(f"t: {t}")
+    print(f"p: {p}")
